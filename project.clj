d0a79d08bc588830cba5b36cdd405df44f92f66c
Radon Rosborough <radon.neon@gmail.com>
2017-07-16 19:32:05 -0700

    Add Leiningen project template

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject raxod502/the-labelizer "0.1.0-SNAPSHOT"
+  :description "Add some jazz to your GitHub issue labels."
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :url "https://github.com/raxod502/the-labelizer"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :main labelizer.core)
