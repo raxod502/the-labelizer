63d4b2e19b42be40fcce33e732f8319afe0a6650
Radon Rosborough <radon.neon@gmail.com>
2017-07-17 21:13:15 -0700

    Add 404 page

--- src/clj/labelizer/app.clj
+++ src/clj/labelizer/app.clj
@@ -1,9 +1,17 @@
 (ns labelizer.app
-  "Ring app to handle HTTP requests.")
+  "Ring app to handle HTTP requests."
+  (:require [bidi.bidi :as bidi]))
+
+(defn response
+  "Construct a Ring response."
+  [status body]
+  {:status status
+   :headers {"Content-Type" "text/html"}
+   :body body})
 
 (defn handler
   "Primary Ring handler for the server."
-  [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello, world!"})
+  [{:keys [uri] :as request}]
+  (if (= uri "/")
+    (response 200 "You have reached the webserver!")
+    (response 404 "Page not found.")))
